5b542b3a4b32d6ce931b49ea433432b180548db0
cp38510 <cp38510@localhost.localdomain>
2018-10-23 13:10:41 +0300

    change script google_cloud_test_settings.sh

--- google_cloud_test_settings.sh
+++ google_cloud_test_settings.sh
@@ -15,7 +15,8 @@ egrep 'PermitRootLogin yes' /etc/ssh/sshd_config && grep 'PasswordAuthentication
 if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, settings in /etc/ssh/sshd_config don't change" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
 systemctl restart ssh
 if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, ssh.service don't restart" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
-sed -i '/root/s/\:\*/\:$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq\//g' /etc/shadow
-grep root /etc/shadow |grep ':$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq/:'
-if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, root passwd don't change in /etc/shadow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+#sed -i '/root/s/\:\*/\:$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq\//g' /etc/shadow
+#grep root /etc/shadow |grep ':$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq/:'
+#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, root passwd don't change in /etc/shadow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOGeXDknImMdgY5xyByU75oyCMQqN94Nq8RPqvLwlaYb3E01AQPwrpj5fV5Vj1GGJ8i+JXh8dG1QmRiwkXUIWZ5VaN6Jkfj9ijgXu3ytboF6W8hwBvhDND70PHgDzcxJ4yRzGCtIZuaIGDZkB10+auMiHyA39+pKw2CIRuqgKugWI/UzuQSbejjBWYdIgJ9AU2AKgt2aQYcjxHccxZLFkCQ3VcTA+mhtcgN69qIlg6TbIfJD9boFjk0G9X0dXoSjjCUZaR6FqbfQK60k+fF02JSydQRZ2lM8t8puOi+QBNrmuxDb73iCTLcYJrN6aCSWUZg10Wx8g0lw/Lq2PU2kNT root" >> /root/.ssh/authorized_keys
 echo "Well done!"
