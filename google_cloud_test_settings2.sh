1dec1d27c53230f69ec8309b8cd99e6484b368e9
cp38510 <34242575+cp38510@users.noreply.github.com>
2019-06-27 13:20:09 +0300

    test
    test

--- google_cloud_test_settings2.sh
+++ google_cloud_test_settings2.sh
@@ -5,18 +5,18 @@
 SCRIPTLOG="/tmp/startscriptlog"
 #
 timedatectl set-timezone Europe/Moscow
-if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, timezone don't set Europe/Moscow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, timezone don't set Europe/Moscow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 apt-get update
-if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, system don't update" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, system don't update" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 apt-get install git less nano nmap -y
-if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, applications don't install" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, applications don't install" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/PermitRootLogin no/PermitRootLogin yes/g'  -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 #egrep 'PermitRootLogin yes' /etc/ssh/sshd_config && grep 'PasswordAuthentication yes' /etc/ssh/sshd_config
-#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, settings in /etc/ssh/sshd_config don't change" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, settings in /etc/ssh/sshd_config don't change" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 systemctl restart ssh
-#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, ssh.service don't restart" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, ssh.service don't restart" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 #sed -i '/root/s/\:\*/\:$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq\//g' /etc/shadow
 #grep root /etc/shadow |grep ':$1$jYePn7UP$1Q6SYWeihp9IWcB3taOeq/:'
-#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, root passwd don't change in /etc/shadow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog" && exit 0; fi
+#if [ $? -ne 0 ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR, root passwd don't change in /etc/shadow" >> ${SCRIPTLOG} && echo "look /tmp/startscriptlog"; fi
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdEuCzFOhJ9pYBJwrs3zgNReFFqEdM6sCiZ1aIlubdle02jlVM1IcTt7Mu+d8X97zVkdjZxCy1KxTgpIRko2eWsjvRJ9XEb8725ZUQw16uJv5V0ZaIaZFTimTO5Hby341B4xTrSWxr8cXjXZ1YZdh3hcYUbQw8yVnOM/UNJ7exa5UaIbvoMKGJGBNy/nII8sRD6wstm+9VdUREe1PMdb8IAigi5q5S5R9kRSA16pkM+kyf4yzcDqD02T4Mwv4Uk2P2uS/dBeDfImq6EAW7NbBOQ/6hfmcZlGWivT91vc5dwMQiXwbBvOfzBDqtQ+Y9N24LHSZYLTAxhAqEyZ60/cJF root" >> /root/.ssh/authorized_keys
 echo "Well done!"
